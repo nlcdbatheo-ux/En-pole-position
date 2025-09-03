7f534d411c963877744431c909eba1b313538579
nlcdbatheo-ux <nlcdbatheo@gmail.com>
2025-09-03 14:51:27 +0200

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,5 @@
-// Ici on définit l'URL de ton backend (Render).
-// ⚠️ Mets bien l’URL de ton backend (pas celle du frontend)
 const config = {
-  API_BASE_URL: "https://en-pole-position-1.onrender.com"
+  API_BASE_URL: "https://en-pole-position-1.onrender.com/backend",
 };
 
 export default config;
